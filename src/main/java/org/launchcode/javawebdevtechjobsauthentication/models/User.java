7aa7dc77fb3c27761c307e400c919a7a5caf10d8
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-06-30 19:19:20 -0500

    start

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,9 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User extends AbstractEntity{
+
+    private String username;
+}
